92b85e972c7d70ec23c50a962cf08da07d237d9f
Tim Goodwin <tlg2132@columbia.edu>
2016-10-30 03:03:59 +0000

    uhh first try for each function implementation

--- machine_learning/hw4/problem2.py
+++ machine_learning/hw4/problem2.py
@@ -1,5 +1,5 @@
 import numpy as np
-# import sklearn as sk
+import math
 from scipy.io import loadmat
 hw4data = loadmat('hw4data.mat')
 
@@ -8,34 +8,62 @@ def main():
     data = hw4data['data']
     lifted_data = np.insert(data, 0, 1, axis=1)
     labels = hw4data['labels']
-    B = np.array([1,3.5,4.3,2.3])
-    compute_objective(2, B, lifted_data, labels)
+    #compute_objective(B, lifted_data, labels)
+    #descend_gradient(B, lifted_data, labels, .0035)
+    gd_handler(lifted_data, labels, 0.65064)
 
-def compute_objective(Bo, B, data, labels):
+def compute_objective(B, data, labels):
     val = 0
     # b is a vector of length 4
     b_vec = B.reshape(1, 4)
     dot = np.dot(b_vec, data.T)
     # data.T is (4, 4096)
-    print dot.shape
+    #print dot.shape
     log_arg = 1 + np.exp(dot)
     log_term = np.log(log_arg)
     second = labels.reshape(1, len(labels)) * dot
     temp = log_term - second
     thesum = np.sum(temp, axis=1)
-    print thesum[0] / len(labels)
+    #print thesum[0] / len(labels)
     return thesum[0] / len(labels)
 
-def descend_gradient(Bo, B, data, labels, stepsize):
-    return 1
+def descend_gradient(B, data, labels, stepsize):
+    errs = np.zeros(B.shape)
+    b_vec = B
+    exp = np.exp(np.dot(b_vec, data.T))
+    #print np.dot(b_vec, data.T)
+    #print exp.shape
+    #print (data * data).T.shape
+    numerator = (data * data) * exp.T
+    #print numerator.shape #(4096, 4)
+    denominator = 1 + exp
+    #print denominator.shape
+    frac = numerator.T / denominator
+    #print frac.shape
+    subt = labels.reshape((1, len(labels))) * data.T
+    #print subt.shape
+    res = frac - subt
+    #print res.shape
+    sums = np.sum(res, axis=1)
+    gradientVal = sums / len(B)
+    #print gradientVal.shape
+
+    # update rule
+    b_vec = b_vec - (gradientVal * stepsize)
+    #print b_vec
+    return b_vec
 
 def gd_handler(data, labels, target):
-    obj = float('inf')
-    B = np.zeros(np.shape(([1, 4])))
+    B = np.zeros((1, 4))
+    print "ok"
+    print B.shape
     steps = 0
     while (steps < 1000):
         steps += 1
-        Beta = descend_gradient(B, data, labels, 10)
+        stepsize = 0.0035 / math.sqrt(steps)
+        B = descend_gradient(B, data, labels, stepsize)
+        obj = compute_objective(B, data, labels)
+        print"OBJECTIVE VALUE", obj
 
 
 if __name__ == "__main__":
