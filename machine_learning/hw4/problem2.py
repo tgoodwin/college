6a5e2ac6a63937c908bf85301c9a297802c37438
Tim Goodwin <tgoodwin@graphiq.com>
2016-10-31 00:48:54 -0400

    cleaned up for part c

--- machine_learning/hw4/problem2.py
+++ machine_learning/hw4/problem2.py
@@ -5,91 +5,70 @@ hw4data = loadmat('hw4data.mat')
 
 def main():
     data = hw4data['data']
-    lifted_data = np.insert(data, 0, 1, axis=1)
+    trans_data = linear_transform(data)
+    print trans_data.shape
+    lifted_data = np.insert(trans_data, 0, 1, axis=1)
     labels = hw4data['labels']
-    #compute_objective(B, lifted_data, labels)
-    #descend_gradient(B, lifted_data, labels, .0035)
     B = np.zeros((1, 4))
-    #print compute_objective(B, lifted_data, labels)
+    #plot_data(lifted_data)
     gd_handler(lifted_data, labels, 0.65064)
 
+def plot_data(data):
+    for i in range(data.shape[0]):
+        print data[i]
+
+def linear_transform(data):
+    A = np.array([[1, 0, 0],[0,12,0],[0,0,1]])
+    return np.dot(data, A)
+
 def compute_objective(B, data, labels):
-    # b is a vector of length 4 after lifting
     b_vec = B.reshape(1, 4)
     dot = np.dot(b_vec, data.T)
-    #print dot
-    # data.T is (4, 4096)
-    #print dot.shape
     log_arg = 1 + np.exp(dot)
-    #print log_arg
     log_term = np.log(log_arg)
-    #print log_term
     second = labels.reshape(1, len(labels)) * dot
     temp = log_term - second
-    B = np.zeros((1, 4))
     thesum = np.sum(temp, axis=1)
-    #print thesum[0] / len(labels)
-    return thesum[0] / len(labels)
+    return np.divide(thesum[0], len(labels))
 
-def descend_gradient(B, data, labels, stepsize):
-    errs = np.zeros(B.shape)
+def descend_gradient(B, data, labels):
     b_vec = B
     exp = np.exp(np.dot(b_vec, data.T))
-    #print np.dot(b_vec, data.T)
-    #print exp.shape
-    #print (data * data).T.shape
-    numerator = (data * data) * exp.T
-    #print numerator.shape #(4096, 4)
+    numerator = data * exp.T
     denominator = 1 + exp
-    #print denominator.shape
-    frac = numerator.T / denominator
-    #print frac.shape
+    frac = np.divide(numerator.T, denominator)
     subt = labels.reshape((1, len(labels))) * data.T
-    #print subt.shape
     res = frac - subt
-    #print res.shape
     sums = np.sum(res, axis=1)
-    gradient_t = sums / len(B)
-    #print gradientVal.shape
+    gradient_t = np.divide(sums, data.shape[0])
+    return gradient_t
 
-    # update rule
-    lambda_norm = np.sqrt(np.dot(gradient_t, gradient_t))
-    b_vec_update = b_vec - (stepsize * gradient_t)
-    b_vec_trial = b_vec_update
-    # backtracking line search for next step size
-    #while (compute_objective(b_vec_trial, data, labels) > (compute_objective(b_vec, data, labels) - 0.5*(stepsize)*(lambda_norm*lambda_norm))):
-     #   stepsize = 0.5 * stepsize
-      #  b_vec_trial = b_vec - (stepsize * gradient_t)
+def line_search(data, labels, gradient_b, B):
+    stepsize = 1
+    lambda_norm = np.dot(gradient_b, gradient_b.T)
+    B_diff = B - (stepsize * gradient_b)
+    obj_orig = compute_objective(B, data, labels)
+    obj_diff = compute_objective(B_diff, data, labels)
 
-    #print b_vec
-    return b_vec_update, stepsize
+    while (obj_diff > obj_orig - (0.5 * stepsize * lambda_norm)):
+        stepsize = 0.5 * stepsize
+        B_diff = B - (stepsize * gradient_b)
+        obj_diff = compute_objective(B_diff, data, labels)
+    return stepsize
 
 def gd_handler(data, labels, target):
     B = np.zeros((1, 4))
     steps = 0
-    stepsize = 0.0035
-    while (steps < 1):
+    stepsize = 1
+    obj = 100
+    while (obj > target):
         steps += 1
-        B, stepsize = descend_gradient(B, data, labels, stepsize)
-        print B, stepsize
+        gradient_b = descend_gradient(B, data, labels)
+        stepsize = line_search(data, labels, gradient_b, B)
+        B = B - (stepsize * gradient_b)
         obj = compute_objective(B, data, labels)
-        print"OBJECTIVE VALUE", obj
-
+        print steps, "OBJECTIVE VALUE", obj, "stepsize", stepsize
 
 if __name__ == "__main__":
 	main()
 
-# write function to compute the value of the objective function
-# write function using the above function to do the gradient descent with the line-search approach
-# write function to count step size while the value of the objective is not less than the assignments requested threshold.
-# try starting with step size of 0.002
-
-
-# while obj > threshold
-# 	steps += 1
-# 	Bo, B = gradientDescent(Bo, B, data, labels, 0.0035/sqrt(steps))
-#	obj = computeObjective(Bo, B, data, labels);
-
-
-# data is 4096x3, add a column of 1s in the front to represent B_o, and now make sure your B vector is of length 4 to match the augmented matrix.
-   
