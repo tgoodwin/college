21fdf0339a065a555374f1e32ffa0b4c7b360ba9
Tim Goodwin <tgoodwin@graphiq.com>
2016-10-31 18:33:58 -0400

    done b

--- machine_learning/hw4/problem2d.py
+++ machine_learning/hw4/problem2d.py
@@ -1,3 +1,8 @@
+# Timothy Goodwin
+# tlg2132
+# 10-31-16
+# Homework 4, problem 2d source code
+
 import numpy as np
 import math
 from scipy.io import loadmat
@@ -5,13 +10,16 @@ hw4data = loadmat('hw4data.mat')
 
 def main():
     data = hw4data['data']
-    data = linear_transform(data)
-    # lift data to combine beta_naught into an expanded solution vector of length 4.
-    lifted_data = np.insert(data, 0, 1, axis=1)
     labels = hw4data['labels']
-    B = np.zeros((1, 4))
-    #inspect_data(lifted_data)
-    gd_handler(lifted_data, labels, 0.65064)
+    PARTITION = int(math.floor(0.8 * data.shape[0]))
+    THRESHOLD = 0.65064
+    data = linear_transform(data)
+    lifted_data = np.insert(data, 0, 1, axis=1)
+    training_data = lifted_data[0:PARTITION]
+    training_labels = labels[0:PARTITION]
+    holdout_data = lifted_data[PARTITION:]
+    holdout_labels = labels[PARTITION:]
+    gd_handler(training_data, training_labels, holdout_data, holdout_labels)
 
 def inspect_data(data):
     for i in range(data.shape[0]):
@@ -44,9 +52,10 @@ def descend_gradient(B, data, labels):
     gradient_t = np.divide(sums, data.shape[0])
     return gradient_t
 
+# determine next stepsize via backtracking line search
 def line_search(data, labels, gradient_b, B):
     stepsize = 1
-    lambda_norm = np.dot(gradient_b, gradient_b.T)
+    lambda_norm = np.dot(gradient_b, gradient_b.T)  # ||lambda||^2 term
     B_diff = B - (stepsize * gradient_b)
     obj_orig = compute_objective(B, data, labels)
     obj_diff = compute_objective(B_diff, data, labels)
@@ -57,18 +66,42 @@ def line_search(data, labels, gradient_b, B):
         obj_diff = compute_objective(B_diff, data, labels)
     return stepsize
 
-def gd_handler(data, labels, target):
-    B = np.zeros((1, 4))
+def is_power_of_two(n):
+    return n != 0 and not ( n & (n - 1))
+
+def record_holdout_rate(B, holdout_data, holdout_labels):
+    signs = np.dot(B, holdout_data.T)
+    preds = np.sign(signs).T
+    errs = 0
+    total = 0
+    for i in range(len(preds)):
+        if int(preds[i][0]) == -1:
+            preds[i][0] = 0
+        if int(preds[i][0]) != int(holdout_labels[i][0]):
+            errs += 1
+        total += 1
+    return float(errs) / float(total)
+
+def gd_handler(data, labels, h_data, h_labels):
+    B = np.zeros((1, 4)) # start at zero as specified
     steps = 0
     stepsize = 1
-    obj = compute_objective(B, data, labels)
-    while (obj > target):
+    best_holdout_rate = float('inf')
+    obj = compute_objective(B, data, labels) #objective value
+    while (1):
         steps += 1
+        if is_power_of_two(steps):
+            holdout_rate = record_holdout_rate(B, h_data, h_labels)
+            print steps, "objective: ", obj, "holdout error rate: ", holdout_rate
+            if (holdout_rate > 0.99 * best_holdout_rate and steps >= 32):
+                break
+            if holdout_rate < best_holdout_rate:
+                best_holdout_rate = holdout_rate
         gradient_b = descend_gradient(B, data, labels)
         stepsize = line_search(data, labels, gradient_b, B)
         B = B - (stepsize * gradient_b)
+        # update objective afer stepping down gradient
         obj = compute_objective(B, data, labels)
-        print steps, "OBJECTIVE VALUE", obj, "stepsize", stepsize
 
 if __name__ == "__main__":
 	main()
