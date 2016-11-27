adbed05194c7c12ec69bbdbb18cd3ecb09b78287
Tim Goodwin <tlg2132@columbia.edu>
2016-11-27 04:04:36 +0000

    gbvv baby

--- machine_learning/hw5/problem1_gbc.py
+++ machine_learning/hw5/problem1_gbc.py
@@ -1,4 +1,3 @@
-import random
 import numpy as np
 import sklearn as sk
 import pandas as pd
@@ -12,45 +11,38 @@ from sklearn.preprocessing import StandardScaler
 
 INPUT_SIZE = 2000
 
-#n_estimators
-#max_depth
-#min_samples_leaf
-
-
 max_depth = [2, 3, 4]
-n_estimators = [50, 100, 150, 200]
 
 cv_params = {'max_depth': [3,5,7], 'min_child_weight': [1,3,5], 'n_estimators': [50, 100, 150]}
-
-optimized_GBM = GridSearchCV(xgb.XGBClassifier(), 
-                            cv_params, 
-                             scoring = 'accuracy', cv = 5, n_jobs = -1, verbose=1)
 
 #n_jobs=-1
 
 def main():
 	# read in csv text, modify training data and labels
 	training = pd.read_csv('reviews_tr.csv')
-	dtrain = xgb.DMatrix(training)
 	training_text = training['text'][0:INPUT_SIZE]
 	training_labels = training['label'][0:INPUT_SIZE].values.reshape((INPUT_SIZE, 1))
-	zeros = np.where(training_labels == 0)
-	training_labels[zeros] = -1
-	del training, zeros
 
 	# read in csv, modify test 
 	test = pd.read_csv('reviews_te.csv')
 	test_text = test['text'][0:INPUT_SIZE]
 	test_labels = test['label'][0:INPUT_SIZE].values.reshape((INPUT_SIZE, 1))
-	zeros = np.where(test_labels == 0)
-	test_labels[zeros] = -1
-	del test, zeros
 
 	unigram_train, unigram_test = build_unigram(training_text, test_text)
+        print unigram_train.shape
+        print unigram_test.shape
+        training = xgb.DMatrix(unigram_train, training_labels)
 
-	clf = GridSearchCV(xgb.XGBClassifier(), cv_params, scoring = 'accuracy', cv = 5, n_jobs = -1, verbose=1)
-	clf.fit(unigram_train, unigram_test)
-	print clf.best_score_, clf.best_params_
+        param = {'max_depth':2, 'eta':0.5, 'silent':1, 'objective':'binary:logistic' }
+
+        clf = xgb.train(param, training, 100)
+
+        tester = xgb.DMatrix(unigram_test, test_labels)
+        raw_preds = clf.predict(tester)
+        binary_preds = np.floor(raw_preds + 0.5)
+        print test_labels.shape
+        print binary_preds.shape
+        print "error", get_error_rate(test_labels, binary_preds)
 
 
 # takes in raw data
@@ -60,6 +52,15 @@ def build_unigram(training_data, test_data):
 	unigram_te = vectorizer.transform(test_data)
 	print "built unigram feature representation"
 	return unigram_tr, unigram_te
+
+def get_error_rate(preds, labels):
+        errorCount = 0
+        totalCount = 0
+        for i in range(len(preds)):
+            if (int(preds[i]) != int(labels[i])):
+                errorCount += 1
+            totalCount += 1
+        return float(errorCount) / float(totalCount)
 
 def select_hyperparams(X, Y, depths, n_estimators):
 	best_score_so_far = 0.0
